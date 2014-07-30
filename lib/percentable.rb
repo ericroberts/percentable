330b213376bfa6c54fe3b17ef75d32bcb4bfcaa2
Eric Roberts <ericroberts@gmail.com>
2014-07-29 21:08:56 -0400

    Add requirement, remove unused code.

--- lib/percentable.rb
+++ lib/percentable.rb
@@ -1,7 +1,4 @@
 require_relative 'percentable/version'
 require_relative 'percentable/numeric'
 require_relative 'percentable/percent'
-
-module Percentable
-  # Your code goes here...
-end
+require_relative 'percentable/percentize'
