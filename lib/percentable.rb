4244e737d61add124c24f9cbd4dac1f8d4d975cd
Eric Roberts <ericroberts@gmail.com>
2014-07-29 20:53:25 -0400

    Require the other percentable files.

--- lib/percentable.rb
+++ lib/percentable.rb
@@ -1,4 +1,6 @@
-require "percentable/version"
+require_relative 'percentable/version'
+require_relative 'percentable/numeric'
+require_relative 'percentable/percent'
 
 module Percentable
   # Your code goes here...
