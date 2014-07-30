0fcff9591d6e8be37a31f0f4b4d3ad2baa71d195
Eric Roberts <ericroberts@gmail.com>
2014-07-30 16:10:56 -0400

    Add some other abilities to percentize - can percentize multiple fields at once - can specify a default for the fields you are percentizing

--- lib/percentable/percentize.rb
+++ lib/percentable/percentize.rb
@@ -1,8 +1,12 @@
 module Percentable
   module Percentize
-    def percentize field
-      define_method(field) do |args=[]|
-        Percent.new(super(*args))
+    def percentize *args
+      options = args.pop if args.last.is_a? Hash
+
+      args.each do |method_name|
+        define_method(method_name) do |args=[]|
+          Percent.new(super(*args) || options[:default])
+        end
       end
     end
   end
